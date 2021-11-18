26ffb0fecc826727399753cf5d891cfb776532ca
Emilee Oquist <emiwemi495@gmail.com>
2021-11-17 17:20:20 -0800

    buttons

--- simone.js
+++ simone.js
@@ -24,3 +24,21 @@
  * 
  * reminder -- this is instead of using <script> so you can get used to working with npm packages
  */
+
+var preRoundDelay = 400,
+    greetingDelay = 12,
+    nextRoundDelay;
+
+// taken- but learning from- David's code
+var play = document.querySelector("#play");
+play.addEventListener("click", function () {
+    var numRounds = parseInt(document.querySelector("#rounds").value, 10),
+    buttons = {};
+  if(!Number.isInteger(numRounds)){
+      numRounds = 10;
+  }
+    buttons.Red = new SimoneButton("#redSq", "sounds/red.wav", "red", "lightred");
+    buttons.Blue = new SimoneButton("#blueSq", "sounds/blue.wav", "blue", "lightblue");
+    buttons.Green = new SimoneButton("#greenSq", "sounds/green.wav", "green", "lightgreen");
+    buttons.Yellow = new SimoneButton("#yellowSq", "sounds/yellow.wav","yellow","lightyellow");
+});
